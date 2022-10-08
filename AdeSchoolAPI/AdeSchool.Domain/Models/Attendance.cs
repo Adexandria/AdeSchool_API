9743de6afecfb9d31b56068d068aa0d76bf097a0
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-10-08 07:23:35 +0100

    protected the date and time

--- AdeSchoolAPI/AdeSchool.Domain/Models/Attendance.cs
+++ AdeSchoolAPI/AdeSchool.Domain/Models/Attendance.cs
@@ -3,7 +3,7 @@
     public class Attendance
     {
         public Guid AttendanceId { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; protected set; }
         public bool IsPresent { get; set; } = false;
         public Student Student { get; set; }
     }
